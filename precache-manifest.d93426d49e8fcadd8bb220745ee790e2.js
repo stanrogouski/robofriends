8c1a1ce95eb4c857f39de882125fe7c6637ab33e
stanrogouski <stanrogouski@gmail.com>
2022-02-02 21:13:52 -0500

    Updates

---
+++ precache-manifest.d93426d49e8fcadd8bb220745ee790e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70c0c4165d51a9b757f89415be29a62",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b2b5d56f3ad38e703ab0",
+    "url": "/robofriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "4afa94f9a64ff61c6adb",
+    "url": "/robofriends/static/css/main.0d807f5f.chunk.css"
+  },
+  {
+    "revision": "b2b5d56f3ad38e703ab0",
+    "url": "/robofriends/static/js/2.e31cd8c6.chunk.js"
+  },
+  {
+    "revision": "4afa94f9a64ff61c6adb",
+    "url": "/robofriends/static/js/main.1f99dcd8.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
